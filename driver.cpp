3e73048ba415767714c7443e0b6f3aa208b2ac3b
Johanna Vo <100493934+johannavo97@users.noreply.github.com>
2023-05-27 22:47:52 -0700

    Update driver.cpp

--- driver.cpp
+++ driver.cpp
@@ -3,8 +3,14 @@
 
 int main() {
   Store myStore;
+  
+  // Reads inventory data from a file
   myStore.readInventory("data4movies.txt");
+  
+  // Reads customer data from a file
   myStore.readCustomers("data4customers.txt");
+  
+    // Reads command data from a file
   myStore.readCommands("data4commands.txt");
   return 0;
 }
